5ed7575dfbbd6902f2c38a2b21fdc6711a2399bb
Md. Ashikur Rahman <48981854+ashik1074@users.noreply.github.com>
2023-05-05 23:22:18 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/*home__title-color*/
+var typed = new Typed(".home__title",{
+    strings:[" ","Undergraduate Student","Front-End Developer","Data Analyst","UI/UX Designer", "Researcher"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop: true
+})
